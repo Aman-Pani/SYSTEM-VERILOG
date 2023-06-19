b065af037f22b3fda7b5b19a3cb9e183b312ce48
Aman Ranjan Pani <85098159+Aman-Pani@users.noreply.github.com>
2023-06-19 21:18:44 +0530

    Update dataType_Enum_exmp01.sv

--- SYSTEM VERILOG/d03/dataType_Enum_exmp01.v
+++ SYSTEM VERILOG/d03/dataType_Enum_exmp01.v
@@ -17,7 +17,7 @@ endmodule
 
 //    0 ,Colors :: Value of red 	   is = 0
 //    1 ,Colors :: Value of green 	 is = 1
-//    2 ,Colors :: Value of blue    is = 2
-//    3 ,Colors :: Value of yellow     is = 3
+//    2 ,Colors :: Value of blue     is = 2
+//    3 ,Colors :: Value of yellow       is = 3
 //    4 ,Colors :: Value of white 	 is = 4
 //    5 ,Colors :: Value of black 	 is = 5
